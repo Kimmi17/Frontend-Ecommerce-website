cd7b1fe9b38a85638c39fef861c8c15b812e2e0b
Kimmi17 <dangthikimthuybl@gmail.com>
2024-03-08 07:30:41 +0200

    Add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-const Footer = () => {
-  return <div>Footer</div>;
+const Footer: React.FC = () => {
+  return (
+    <footer
+      className="bg-gradient-to-r from-c5eff7 to-96d6e0 py-8 px-8 text-center bottom-0 w-full"
+      style={{
+        background: "linear-gradient(to right, #c5eff7, #96d6e0)",
+        marginTop: "20px",
+      }}
+    >
+      <p className="text-black font-serif font-bold">
+        &copy; 2024 Dang Thuy. All rights reserved. Photo from Vascara's
+        collection
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
